d327166469e9bf6a09ff0a49875417afe99bda6f
Eduardo Derisso <dudu.derisso@gmail.com>
2024-09-17 15:50:55 -0300

    feat[modais]: added missing inputs

--- src/components/modais/login/loginModal.js
+++ src/components/modais/login/loginModal.js
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import "./loginModalStyle.css"
 
 export default function LoginModal({setIsModalLoginOpen}) {
 
+    const [email, setEmail] = useState("");
+    const [userType, setUserType ] = useState("");
+    const [password, setPassword] = useState("");
+
+
+
     function handleClose(event) {
         setIsModalLoginOpen(false);
-        
+    }
+
+    function handleLogin() {
+        console.log("email: ", email, "| tipo: ", userType, " | password: ", password);
+        console.log("fazer hook login");
     }
 
     return (
@@ -13,22 +24,22 @@ export default function LoginModal({setIsModalLoginOpen}) {
             <div className="modal-title">
                     <h2>Login</h2>
                 </div>
-                <label> username:
-                    <input type="text" name="username" required />
+                <label> email:
+                    <input type="text" name="email" onChange={(e) => setEmail(e.target.value)} required />
                 </label>
                 <label> tipo de usuário:
-                    <select name="user_type" id="user_type">
+                    <select name="user_type" onChange={(e) => setUserType(e.target.value)}>
                         <option value="paciente">paciente</option>
                         <option value="medico">médico</option>
                         <option value="administrador">administrador</option>
                     </select>
                 </label>
                 <label> password:
-                    <input type="password" name="password" required />
+                    <input type="password" name="password" onChange={(e) => setPassword(e.target.value)} required />
                 </label>
                 <div className="button-area">
                     <button onClick={handleClose}>cancel</button>
-                    <button>login</button>
+                    <button onClick={handleLogin}>login</button>
                 </div>
             </div>
         </div>
--- src/components/modais/signIn/signInModal.js
+++ src/components/modais/signIn/signInModal.js
@@ -1,91 +1,123 @@
 import { useState } from "react";
-import "./signInModalStyle.css"
+import "./signInModalStyle.css";
 
-export default function SignInModal({setIsModalSignInOpen}) {
+export default function SignInModal({ setIsModalSignInOpen }) {
 
+    const [nome, setNome] = useState("");
+    const [email, setEmail] = useState("");
+    const [sexo, setSexo] = useState("");
     const [userType, setUserType] = useState("");
+    const [cri, setCri] = useState("");
+    const [dataNascimento, setDataNascimento] = useState("")
+    const [especialidade, setEspecialidade] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [cpf, setCpf] = useState("");
+    const [estadoCivil, setEstadoCivil] = useState("");
 
     function handleClose(event) {
         setIsModalSignInOpen(false);
     }
 
     function handleTypeSelection(event) {
-            console.log(event.target.value)
-    }   
+        setUserType(event.target.value);
+    }
+
+    function handleFormSubmit(event) {
+        event.preventDefault();
+            console.log({ nome, email, sexo, userType, dataNascimento, cri, especialidade, password, cpf, estadoCivil });
+    }
 
     return (
-        <div className="signin-modal__backgroud" onClick={(event) => {handleClose(event)}}>
+        <div className="signin-modal__background" onClick={(event) => handleClose(event)}>
             <div className="signin-modal__container" onClick={e => e.stopPropagation()}>
                 <div className="modal-title">
                     <h2>Sign in</h2>
                 </div>
-                <label>nome:
-                    <input type="text" name="username" required />
-                </label>
-
-                <label>sexo:
-                    <select name="sexo" id="sexo">
-                        <option value="faminino">feminino</option>
-                        <option value="masculino">masculino</option>
-                    </select>
-                </label>
-                <label>tipo de usuário:
-                    <select name="user_type" id="user_type" onChange={(event) => handleTypeSelection(event)}>
-                        <option value="paciente">paciente</option>
-                        <option value="medico">médico</option>
-                        <option value="administrador">administrador</option>
-                    </select>
-                </label>
-
-                {userType == "medico" ?
-                    <>
-                        <label>CRMI:
-                            <input type="text" name="crmi" required />
+                    <form onSubmit={handleFormSubmit} className="signin-modal__form">
+                        <label>nome:
+                            <input type="text" name="nome" required value={nome} onChange={(e) => setNome(e.target.value)} />
                         </label>
 
-                        <label>especialidade:
-                            <select name="especialidade">
-                                <option value="cardiologista">cardiologista</option>
-                                <option value="urologia">urologia</option>
-                                <option value="neurologia">neurologia</option>
+                        <label>email:
+                            <input type="email" name="email" required value={email} onChange={(e) => setEmail(e.target.value)} />
+                        </label>
+
+                        <label>sexo:
+                            <select name="sexo" id="sexo" value={sexo} onChange={(e) => setSexo(e.target.value)} required>
+                                <option value="">Selecione...</option>
+                                <option value="feminino">feminino</option>
+                                <option value="masculino">masculino</option>
                             </select>
                         </label>
-                    </>
-                    :
-                    <></>
-                }
 
-                {userType == "paciente" ?
-                    <>
-                        <label>CPF:
-                            <input type="text" name="cpf" required />
+                        <label>data de nascimento:
+                            <input type="date" value={dataNascimento} onChange={(e) => setDataNascimento(e.target.value)} />
                         </label>
 
-                        <label>estado civil:
-                            <select name="estado-civil">
-                                <option value="solteiro">solteiro</option>
-                                <option value="casado">casado</option>
-                                <option value="divorciado">divorciado</option>
-                                <option value="viuvo">viuvo</option>
+
+                        <label>tipo de usuário:
+                            <select name="user_type" id="user_type" onChange={handleTypeSelection} value={userType} required>
+                                <option value="">Selecione...</option>
+                                <option value="paciente">paciente</option>
+                                <option value="medico">médico</option>
                             </select>
                         </label>
-                    </>
-                    :
-                    <></>
-                }
 
-                <label>senha:
-                    <input type="password" name="password" required />
-                </label>
-                <label>confirme a senha:
-                    <input type="confirm_password" name="confirm_password" required />
-                </label>
+                        {userType === "medico" && (
+                            <>
+                                <label>CRI:
+                                    <input type="text" name="cri" value={cri} onChange={(e) => setCri(e.target.value)} required />
+                                </label>
 
-                <div className="button-area">
-                    <button onClick={handleClose}>cancel</button>
-                    <button >sign in</button>
-                </div>
+                                <label>especialidade:
+                                    <select name="especialidade" value={especialidade} onChange={(e) => setEspecialidade(e.target.value)} required>
+                                        <option value="">Selecione...</option>
+                                        <option value="cardiologista">cardiologista</option>
+                                        <option value="urologia">urologia</option>
+                                        <option value="neurologia">neurologia</option>
+                                    </select>
+                                </label>
+                            </>
+                        )}
+
+                        {userType === "paciente" && (
+                            <>
+                                <label>CPF:
+                                    <input type="text" name="cpf" value={cpf} onChange={(e) => setCpf(e.target.value)} required />
+                                </label>
+
+                                <label>estado civil:
+                                    <select name="estado-civil" value={estadoCivil} onChange={(e) => setEstadoCivil(e.target.value)} required>
+                                        <option value="">Selecione...</option>
+                                        <option value="solteiro">solteiro</option>
+                                        <option value="casado">casado</option>
+                                        <option value="divorciado">divorciado</option>
+                                        <option value="viuvo">viúvo</option>
+                                    </select>
+                                </label>
+                            </>
+                        )}
+
+                        {userType != "" && (
+                            <>
+                                <label>senha:
+                                    <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                                </label>
+
+                                <label>confirme a senha:
+                                    <input type="password" name="confirm_password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} required />
+                                </label>
+                            </>
+                        )}
+                        
+
+                        <div className="button-area">
+                            <button type="button" onClick={handleClose}>cancel</button>
+                            <button type="submit">sign in</button>
+                        </div>
+                    </form>
             </div>
         </div>
-    )
+    );
 }
