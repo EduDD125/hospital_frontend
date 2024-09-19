ae46c59b514ba3544f6a4eac13a61807cbfa5d5c
Eduardo Derisso <dudu.derisso@gmail.com>
2024-09-19 15:42:47 -0300

    feat[hooks login e sing  up]: criado hooks de cadastro e login

--- src/hooks/login/useLogin.js
+++ src/hooks/login/useLogin.js
@@ -4,33 +4,34 @@ import apiClient from '../../axios/apiClient'
 export default function useLogin() {
     const [endpoint, setEndpoint] = useState();
 
-    function login({email, senha, tipo}) {
 
-        if (tipo === "medico") setEndpoint("/login/medico")
-        else if (tipo === "paciente") setEndpoint("/login/paciente")
+    async function login(email, senha, tipo) {
 
-        try {
-            if(tipo=="paciente") {
-                apiClient.post(endpoint, {
-                    email: email,
-                    senha: senha
-                } )
-                    .then( response => {
-                        console.log("Login bem sucedido: ", response.data);
-                        const token = response.data.token;
-                        localStorage.setItem("token", token)
-                    })
-                    .catch(error => {
-                        if(error.response)
-                            console.log("Error ao fazer login:", error.response.data.message)
-                        else
-                            console.log("Erro de rede ou outro: ", error.message)
-                    })
+        console.log("tipo: ", tipo, " email: ", email, " senha: ", senha)
+        if (tipo == "medico") setEndpoint("/login/medico");
+        else if (tipo == "paciente") setEndpoint("/login/paciente");
+        else if (tipo == "administrador") setEndpoint("/login/admin");
+        else {
+            console.log("tipo de usuário desconhecido");
+            //return
         }
-        }catch (error) {
-            console.log(error);
-        }
+
+        apiClient.post(endpoint, {
+            email: email,
+            senha: senha
+        } )
+            .then( response => {
+                console.log("Login bem sucedido: ", response.data);
+                const token = response.data.token;
+                localStorage.setItem("token", token)
+            })
+            .catch(error => {
+                if(error.response)
+                    console.log("Error ao fazer login:", error.response.data.message)
+                else
+                    console.log("Erro de rede ou outro: ", error.message)
+            })
     }
 
-    return login();
+    return login;
 }
---
+++ src/hooks/signUp/useSignUp.js
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import apiClient from "../../axios/apiClient"
+import axios from 'axios';
+
+export const useCreatePaciente = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+  const [endpoint, setEndPoint] = useState(null);
+
+  const createUser = async (userData, tipo) => {
+
+    if (tipo === "paciente") setEndPoint("/pacientes");
+    else if (tipo === "medico") setEndPoint("/medicos");
+    else console.log("Tipo de usuário desconhecido");
+
+
+    setLoading(true);
+    try {
+      const response = await apiClient.post(endpoint, userData);
+      setData(response.data);
+      console.log("data: ", data)
+      setLoading(false);
+    } catch (err) {
+      setError(err.response ? err.response.data : 'Erro ao criar paciente');
+      setLoading(false);
+    }
+  };
+
+  return { createUser, data, loading, error };
+};
