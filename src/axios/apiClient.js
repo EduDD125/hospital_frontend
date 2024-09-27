dc1365f1bc1330ae6eed00f453384f9e3ce7ad84
Eduardo Derisso <dudu.derisso@gmail.com>
2024-09-26 22:30:12 -0300

    feat[axios]: mudado url base para api online

--- src/axios/apiClient.js
+++ src/axios/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://hospital-backend-j5hu.onrender.com',
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
